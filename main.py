c7e9bc2a33b1c423de7df381903c8d577e96ede5
SandroContreras <165609852+SandroContreras@users.noreply.github.com>
2025-06-15 20:53:58 -0700

    Transformed Code into Functions
    I took loose code and created functions to clean up the While Loop sequence.

--- main.py
+++ main.py
@@ -36,6 +36,74 @@ windowSize = 20
 i = 0
 total = 0
 SMA = 0
+def BatteryVoltage_SMA():
+    if len(BatteryVoltageArr) == windowSize:	## Append to Array until window size is met
+        for num in BatteryVoltageArr:			## Sum up all  Array Values
+            total += num
+            SMA = total / windowSize
+    BatteryVoltageArr.pop(0)				## Remove the first Array Index Value
+    time.sleep(0.005)						## Delay for 5 ms
+    battery_voltage = SMA						## Now the battery_voltage is stable
+    return battery_voltage
+
+def SOCtable(battery_voltage):
+    ## Create State of Charge Table
+    if 3.0 <= battery_voltage <= 3.10:
+        battery_percentage = 5
+        return battery_percentage
+    elif 3.10 <= battery_voltage <= 3.20:
+        battery_percentage = 7
+        return battery_percentage
+    elif 3.20 <= battery_voltage <= 3.30:
+        battery_percentage = 10
+        return battery_percentage
+    elif 3.30 <= battery_voltage <= 3.35:
+        battery_percentage = 13
+        return battery_percentage
+    elif 3.35 <= battery_voltage <= 3.40:
+        battery_percentage = 15
+        return battery_percentage
+    elif 3.40 <= battery_voltage <= 3.50:
+        battery_percentage = 20
+        return battery_percentage
+    elif 3.50 <= battery_voltage <= 3.55:
+        battery_percentage = 30
+        return battery_percentage
+    elif 3.55 <= battery_voltage <= 3.60:
+        battery_percentage = 40
+        return battery_percentage
+    elif 3.60 <= battery_voltage <= 3.70:
+        battery_percentage = 50
+        return battery_percentage
+    elif 3.70 <= battery_voltage <= 3.80:
+        battery_percentage = 60
+        return battery_percentage
+    elif 3.80 <= battery_voltage <= 3.90:
+        battery_percentage = 70
+        return battery_percentage
+    elif 3.90 <= battery_voltage <= 4.0:
+        battery_percentage = 80
+        return battery_percentage
+    elif 4.0 <= battery_voltage <= 4.1:
+        battery_percentage = 90
+        return battery_percentage
+    elif 4.1 <= battery_voltage <= 4.2:
+        battery_percentage = 100
+        return battery_percentage
+
+def OledSignal(previous_battery_voltage, battery_voltage):
+    ## Create Power Bank Interaction Signal
+    ## The Oled will only display when the charging bank is Charging or Providing Charge
+    if (previous_battery_voltage < battery_voltage or previous_battery_voltage > battery_voltage):
+        
+        oled.fill(0)
+        oled.text("Battery: ", 0, 0)
+        oled.text(percentSymbol, 80, 0)
+        oled.text(battery_percent_str, 65, 0)
+        oled.show()
+    else:		## If the power bank is idle then power off the oled
+        oled.poweroff()
+        
 while True:
     raw = adc.read_u16()	## Read a Raw analog value in the range 0 - 65535
     ## Step 1: Find Adc Voltage
@@ -46,72 +114,18 @@ while True:
     ## R1 = 47k ohm, R2 = 100k ohm
     battery_voltage = adc_voltage * 1.47
     
-    ## Step 3: Use battery voltage to find Battery Charge Percentage
+    battery_percentage = 0
     
-    ## Formula for converting voltage to Battery Health Percentage: Percentage = [(Actual Voltage - Minimum Voltage) / (Maximum Voltage - Minimum Voltage)] * 100
-        ## Save this code for future Fuel Guage Chips
-    #percentage = (((battery_voltage - 2.5)/(4.2 - 2.5)) * 100)
-    #percentage_int = int(percentage)
-    #percentage_str = str(percentage_int)
-    
-    battery_percentage = 95
-    
-    ## Create State of Charge Table
-    if 3.0 <= battery_voltage <= 3.10:
-        battery_percentage = 5
-    elif 3.10 <= battery_voltage <= 3.20:
-        battery_percentage = 7
-    elif 3.20 <= battery_voltage <= 3.30:
-        battery_percentage = 10
-    elif 3.30 <= battery_voltage <= 3.35:
-        battery_percentage = 13
-    elif 3.35 <= battery_voltage <= 3.40:
-        battery_percentage = 15
-    elif 3.40 <= battery_voltage <= 3.50:
-        battery_percentage = 20
-    elif 3.50 <= battery_voltage <= 3.55:
-        battery_percentage = 30
-    elif 3.55 <= battery_voltage <= 3.60:
-        battery_percentage = 40
-    elif 3.60 <= battery_voltage <= 3.70:
-        battery_percentage = 50
-    elif 3.70 <= battery_voltage <= 3.80:
-        battery_percentage = 60
-    elif 3.80 <= battery_voltage <= 3.90:
-        battery_percentage = 70
-    elif 3.90 <= battery_voltage <= 4.0:
-        battery_percentage = 80
-    elif 4.0 <= battery_voltage <= 4.1:
-        battery_percentage = 90
-    elif 4.1 <= battery_voltage <= 4.2:
-        battery_percentage = 100
-        
     ## Use SMA to smoothen out the battery percentage
     BatteryVoltageArr.append(battery_voltage)
+
+    BatteryVoltage_SMA()
     
-    if len(BatteryVoltageArr) == windowSize:	## Append to Array until window size is met
-        for num in BatteryVoltageArr:			## Sum up all  Array Values
-            total += num
-        SMA = total / windowSize
-        BatteryVoltageArr.pop(0)				## Remove the first Array Index Value
-        time.sleep(0.005)						## Delay for 5 ms
-     battery_voltage = SMA						## Now the battery_voltage is stable   
+    SOCtable(battery_voltage)
+     
     battery_percent_str = str(battery_percentage)
     
-    ## Create Power Bank Interaction Signal
-    ## The Oled will only display when the charging bank is Charging or Providing Charge
-    #startTime = time.time()	# Start timing the battery voltage status
-    if (previous_battery_voltage < battery_voltage or previous_battery_voltage > battery_voltage):
-        
-        oled.fill(0)
-        oled.text("Battery: ", 0, 0)
-        oled.text(percentSymbol, 80, 0)
-        oled.text(battery_percent_str, 65, 0)
-        #oled.text(percentage_str, 65, 0)
-        
-        oled.show()
-    else:		## If the power bank is idle then power off the oled
-        oled.poweroff()
+    OledSignal(previous_battery_voltage, battery_voltage)
         
     previous_battery_voltage = battery_voltage	## Update the lower bounds to avoid an always on state
     
