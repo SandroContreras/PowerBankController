01fc0c6e33143ced62d60beaca550403b0310709
SandroContreras <165609852+SandroContreras@users.noreply.github.com>
2025-06-29 22:41:24 -0700

    Program Functions
    The Program appears to function within the IDE. There are no more errors. I started the parallel list for the voltage and percentage comparisons.

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ adc = ADC(Pin(26))	## GPIO 26 on Pico is an ADC0 Pin
 percentSymbol = "%"
 battery_percent_str = ""
 
-BatteryVoltageArr = []
-movingAvg = []
-windowSize = 12 
+# BatteryVoltageArr = []
+# movingAvg = []
+#windowSize = 12 
 i = 0
 SMA = 0
 battery_percentage = 0		## Default percentage
@@ -34,18 +34,21 @@ window_average = 0
 previous_battery_voltage = 0
 battery_voltage = 0
 class BatteryManager:
-    def __init__(self, raw, adc_voltage, movingAvg, windowSize, i, SMA, window_average, BatteryVoltageArr, battery_voltage, battery_percentage):
+    def __init__(self, raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage):
         self.raw = raw
         self.adc_voltage = adc_voltage
-        self.movingAvg = movingAvg
-        self.windowSize = windowSize
+        self.movingAvg = []
+        self.windowSize = 12
         self.i = i
         self.SMA = SMA
         self.window_average = window_average
-        self.BatteryVoltageArr = BatteryVoltageArr
+        self.BatteryVoltageArr = []
         self.battery_voltage = battery_voltage
         self.battery_percentage = battery_percentage
         
+    def AppendArray(self, element):
+        self.BatteryVoltageArr.append(element)
+    
     def PowerCalculator(self):						## Values 0 - 65535 represents voltages between 0V - 3.3V
         self.raw = adc.read_u16()					## Read a Raw analog value in the range 0 - 65535
 
@@ -59,70 +62,72 @@ class BatteryManager:
 
     def BatteryVoltage_SMA(self, battery_voltage):
         print("Function triggered")
-        for item in BatteryVoltageArr:
+        for item in self.BatteryVoltageArr:
             print(item)
-        while i < len(BatteryVoltageArr) - windowSize + 1:	## Append to Array until window size is met
+        while self.i < len(self.BatteryVoltageArr) - self.windowSize + 1:	## Append to Array until window size is met
             print("While statement triggered")
             
-            window_average = round(sum(BatteryVoltageArr) / windowSize, 2)
-            battery_voltage = window_average	## battery_voltage = SMA of Battery Voltages
+            self.window_average = round(sum(self.BatteryVoltageArr) / self.windowSize, 2)
+            self.battery_voltage = self.window_average	## battery_voltage = SMA of Battery Voltages
             
-            movingAvg.append(battery_voltage)	## Store SMA in SMA array
-            print("SMA Battery Voltage:", battery_voltage)
-            BatteryVoltageArr.pop(0)
+            self.movingAvg.append(self.battery_voltage)	## Store SMA in SMA array
+            print("SMA Battery Voltage:", self.battery_voltage)
+            self.BatteryVoltageArr.pop(0)
 
-        return battery_voltage
+        return self.battery_voltage
     
-    @staticmethod
-    def SOCtable(battery_voltage):
+    #@staticmethod
+    def SOCtable(self, battery_voltage):
+    VoltageRange = [3.0, 3.1, 3.2, 3.3]		## Range of voltages
+    output       = [       5,   7,  10]
     ## Create State of Charge Table
     ######################################  TURN THIS INTO A BISECT + LOOKUP LIST FOR OPTIMIZATION ###########################################################################
         if 3.0 <= battery_voltage <= 3.10:
-            battery_percentage = 5
-            return battery_percentage
+            self.battery_percentage = 5
+            return self.battery_percentage
         elif 3.10 <= battery_voltage <= 3.20:
-            battery_percentage = 7
-            return battery_percentage
+            self.battery_percentage = 7
+            return self.battery_percentage
         elif 3.20 <= battery_voltage <= 3.30:
-            battery_percentage = 10
-            return battery_percentage
+            self.battery_percentage = 10
+            return self.battery_percentage
         elif 3.30 <= battery_voltage <= 3.35:
-            battery_percentage = 13
-            return battery_percentage
+            self.battery_percentage = 13
+            return self.battery_percentage
         elif 3.35 <= battery_voltage <= 3.40:
-            battery_percentage = 15
-            return battery_percentage
+            self.battery_percentage = 15
+            return self.battery_percentage
         elif 3.40 <= battery_voltage <= 3.50:
-            battery_percentage = 20
-            return battery_percentage
+            self.battery_percentage = 20
+            return self.battery_percentage
         elif 3.50 <= battery_voltage <= 3.55:
-            battery_percentage = 30
-            return battery_percentage
+            self.battery_percentage = 30
+            return self.battery_percentage
         elif 3.55 <= battery_voltage <= 3.60:
-            battery_percentage = 40
-            return battery_percentage
+            self.battery_percentage = 40
+            return self.battery_percentage
         elif 3.60 <= battery_voltage <= 3.70:
-            battery_percentage = 50
-            return battery_percentage
+            self.battery_percentage = 50
+            return self.battery_percentage
         elif 3.70 <= battery_voltage <= 3.80:
-            battery_percentage = 60
-            return battery_percentage
+            self.battery_percentage = 60
+            return self.battery_percentage
         elif 3.80 <= battery_voltage <= 3.90:
-            battery_percentage = 70
-            return battery_percentage
+            self.battery_percentage = 70
+            return self.battery_percentage
         elif 3.90 <= battery_voltage <= 4.0:
-            battery_percentage = 80
-            return battery_percentage
+            self.battery_percentage = 80
+            return self.battery_percentage
         elif 4.0 <= battery_voltage <= 4.1:
             self.battery_percentage = 90
-            return battery_percentage
+            return self.battery_percentage
         elif 4.1 <= battery_voltage <= 4.2:
-            battery_percentage = 100
-            return battery_percentage
+            self.battery_percentage = 100
+            return self.battery_percentage
 
 class OledUI(BatteryManager):		## Inherit the variables from BatteryManager Class
     def __init__(self, previous_battery_voltage, battery_percent_str, oled):
-        super().__init__(raw, adc_voltage, movingAvg, windowSize, i, SMA, window_average, BatteryVoltageArr, battery_voltage, battery_percentage)
+        super().__init__(raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage)
         self.battery_percent_str = battery_percent_str
         self.oled = oled
         self.previous_battery_voltage = previous_battery_voltage
@@ -130,7 +135,7 @@ class OledUI(BatteryManager):		## Inherit the variables from BatteryManager Clas
     def variableUpdater(self, previous_battery_voltage, battery_voltage):
         self.previous_battery_voltage = self.battery_voltage
     
-    def OledSignal(self, previous_battery_voltage, percentSymbol, battery_voltage):
+    def OledSignal(self, previous_battery_voltage, percentSymbol, battery_voltage, battery_percent_str):
     ## Create Power Bank Interaction Signal
     ## The Oled will only display when the charging bank is Charging or Providing Charge
         if (self.previous_battery_voltage < self.battery_voltage or self.previous_battery_voltage > self.battery_voltage):
@@ -200,7 +205,7 @@ class OledUI(BatteryManager):		## Inherit the variables from BatteryManager Clas
         else:		## If the power bank is idle then power off the oled
             self.oled.poweroff()
         
-BatteryMethods = BatteryManager(raw, adc_voltage, movingAvg, windowSize, i, SMA, window_average, BatteryVoltageArr, battery_voltage, battery_percentage)
+BatteryMethods = BatteryManager(raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage)
 OledMethods = OledUI(previous_battery_voltage, battery_percent_str, oled)
 while True:
     
@@ -209,7 +214,8 @@ while True:
     battery_voltage = BatteryMethods.PowerCalculator()
     
     ## Use SMA to smoothen out the battery percentage
-    BatteryVoltageArr.append(battery_voltage)
+    BatteryMethods.AppendArray(battery_voltage)
+    #BatteryVoltageArr.append(battery_voltage)
 
     battery_voltage = BatteryMethods.BatteryVoltage_SMA(battery_voltage)
    
@@ -217,6 +223,6 @@ while True:
      
     battery_percent_str = str(battery_percentage)
     
-    OledMethods.OledSignal(previous_battery_voltage, percentSymbol, battery_voltage)
+    OledMethods.OledSignal(previous_battery_voltage, percentSymbol, battery_voltage, battery_percent_str)
     
-    previous_battery_voltage = battery_voltage	## Update the lower bounds to avoid an always on state
+    OledMethods.variableUpdater(previous_battery_voltage, battery_voltage)	# Update the lower bounds to avoid an always on state
