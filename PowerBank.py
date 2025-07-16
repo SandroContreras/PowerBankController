c18b11b914659b0e130b40b64fd7dbc6457bd14a
SandroContreras <165609852+SandroContreras@users.noreply.github.com>
2025-07-15 17:02:12 -0700

    OLED Display Functions
    Now the OLED is displaying as directed in the OledSignal Method

--- PowerBank.py
+++ PowerBank.py
@@ -1,11 +1,11 @@
-i = 0
-SMA = 0
-battery_percentage = 0		## Default percentage
-raw = 0
-adc_voltage = 0
-window_average = 0
-previous_battery_voltage = 0
-battery_voltage = 0
+from machine import Pin, I2C
+import ssd1306
+import time
+from machine import ADC
+
+
+## Create ADC object on an ADC pin
+adc = ADC(Pin(26))	## GPIO 26 on Pico is an ADC0 Pin
 class BatteryManager:
     def __init__(self, raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage):
         self.raw = raw
@@ -25,18 +25,16 @@ class BatteryManager:
     def PowerCalculator(self):						## Values 0 - 65535 represents voltages between 0V - 3.3V
         self.raw = adc.read_u16()					## Read a Raw analog value in the range 0 - 65535
 
-        self.adc_voltage = (raw * 3.3 / 65535)		## Step 1: Find Adc Voltage
+        self.adc_voltage = (self.raw * 3.3 / 65535)		## Step 1: Find Adc Voltage
 
         self.battery_voltage = self.adc_voltage * 1.47 	## Battery Voltage = Adc Voltage * ((R1+R2) / R2)
+        print("Calculated Battery Voltage: ", self.battery_voltage)
         return self.battery_voltage					## R1 = 47k ohm, R2 = 100k ohm
-
-#     def	BatteryVoltageArrAppender(self.battery_voltage):
-#         self.BatteryVoltageArr.append(self.battery_voltage)
 
     def BatteryVoltage_SMA(self, battery_voltage):
         print("Function triggered")
         for item in self.BatteryVoltageArr:
-            print(item)
+            print("Voltages in Battery Voltage Array: ", item)
         while self.i < len(self.BatteryVoltageArr) - self.windowSize + 1:	## Append to Array until window size is met
             print("While statement triggered")
             
@@ -49,88 +47,79 @@ class BatteryManager:
 
         return self.battery_voltage
     
-    #@staticmethod
     def SOCtable(self, battery_voltage):
     #VoltageRange = [3.0, 3.1, 3.2, 3.3]		## Range of voltages
     #output       = [       5,   7,  10]
     ## Create State of Charge Table
     ######################################  TURN THIS INTO A BISECT + LOOKUP LIST FOR OPTIMIZATION ###########################################################################
         if 3.0 <= battery_voltage <= 3.10:
-            self.battery_percentage = 5
-            return self.battery_percentage
+            return 5
         elif 3.10 <= battery_voltage <= 3.20:
-            self.battery_percentage = 7
-            return self.battery_percentage
+            return 7
         elif 3.20 <= battery_voltage <= 3.30:
-            self.battery_percentage = 10
-            return self.battery_percentage
+            return 10
         elif 3.30 <= battery_voltage <= 3.35:
-            self.battery_percentage = 13
-            return self.battery_percentage
+            return 13
         elif 3.35 <= battery_voltage <= 3.40:
-            self.battery_percentage = 15
-            return self.battery_percentage
+            return 15
         elif 3.40 <= battery_voltage <= 3.50:
-            self.battery_percentage = 20
-            return self.battery_percentage
+            return 20
         elif 3.50 <= battery_voltage <= 3.55:
-            self.battery_percentage = 30
-            return self.battery_percentage
+            return 30
         elif 3.55 <= battery_voltage <= 3.60:
-            self.battery_percentage = 40
-            return self.battery_percentage
+            return 40
         elif 3.60 <= battery_voltage <= 3.70:
-            self.battery_percentage = 50
-            return self.battery_percentage
+            return 50
         elif 3.70 <= battery_voltage <= 3.80:
-            self.battery_percentage = 60
-            return self.battery_percentage
+            return 60
         elif 3.80 <= battery_voltage <= 3.90:
-            self.battery_percentage = 70
-            return self.battery_percentage
+            return 70
         elif 3.90 <= battery_voltage <= 4.0:
-            self.battery_percentage = 80
-            return self.battery_percentage
+            return 80
         elif 4.0 <= battery_voltage <= 4.1:
-            self.battery_percentage = 90
-            return self.battery_percentage
+            return 90
         elif 4.1 <= battery_voltage <= 4.2:
-            self.battery_percentage = 100
-            return self.battery_percentage
+            return 100
 
-class OledUI(BatteryManager):		## Inherit the variables from BatteryManager Class
-    def __init__(self, previous_battery_voltage, battery_percent_str, oled):
-        super().__init__(raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage)
+class OledUI(BatteryManager):		## Inherit the variables from	 BatteryManager Class
+    def __init__(self, previous_battery_voltage, battery_percent_str, oled, battery_voltage, battery_percentage, raw, adc_voltage):
+        dummy_raw = 0
+        dummy_i = 0
+        dummy_SMA = 0
+        dummy_window_average = 0
+        dummy_adc_voltage = 0
+        
+        super().__init__(dummy_raw, dummy_adc_voltage, dummy_i, dummy_SMA, dummy_window_average, battery_voltage, battery_percentage)		## I only need variables: battery_voltage, battery_percentage from parent class
         self.battery_percent_str = battery_percent_str
         self.oled = oled
-        self.previous_battery_voltage = previous_battery_voltage
-    
+        #self.previous_battery_voltage = previous_battery_voltage
+        self.percentSymbol = "%"
     def variableUpdater(self, previous_battery_voltage, battery_voltage):
         self.previous_battery_voltage = self.battery_voltage
     
     def OledSignal(self, previous_battery_voltage, percentSymbol, battery_voltage, battery_percent_str):
     ## Create Power Bank Interaction Signal
     ## The Oled will only display when the charging bank is Charging or Providing Charge
-        if (self.previous_battery_voltage < self.battery_voltage or self.previous_battery_voltage > self.battery_voltage):
+        #if (3.81 <= self.battery_voltage <= 3.83):
+        print("previous_battery_voltage value in OledSignal: ", previous_battery_voltage)
+        print("battery_voltage value in OledSignal: ", battery_voltage)
+        if (previous_battery_voltage < battery_voltage or previous_battery_voltage > battery_voltage):
         
             self.oled.fill(0)
             self.oled.text("Sandro's", 30, 30, 1)		## Signature
             self.oled.text("Power Bank", 20, 40, 1)
 
-            if (self.battery_voltage >= 3.20):
+            if (battery_voltage >= 3.20):
                 ## Display Battery Percentage for Double Digit Value
                 self.oled.text(self.percentSymbol, 86, 4)
                 self.oled.text(self.battery_percent_str, 70, 4)
             else:
                 ## Shift Percent Symbol inwards towards the Single Digit Value
-                self.oled.text(self.percentSymbol, 78, 4)
+                self.oled.text(percentSymbol, 78, 4)
                 self.oled.text(self.battery_percent_str, 70, 4)
         
-            print("Previous Battery Voltage: ", self.previous_battery_voltage)
-            print("Current Battery Voltage: ", self.battery_voltage)
-        
             ## The Charging symbol will only appear if the Bank is Charging
-            if (self.previous_battery_voltage < self.battery_voltage):
+            if (previous_battery_voltage < battery_voltage):
                 ## Create Charging Symbol
                 self.oled.vline(55, 0, 16, 1)
                 self.oled.hline(55, 6, 8, 1)	## Create horizontal line at halfway point at charging symbol
@@ -155,19 +144,19 @@ class OledUI(BatteryManager):		## Inherit the variables from BatteryManager Clas
             self.oled.rect(41, 3, 3, 5, 1)	## Battery Terminal Symbol
         
             ## Create if statements to dicate which inner battery quadrants to fill in depending on battery health levels
-            if (3.0 <= self.battery_voltage <= 3.55):
+            if (3.0 <= battery_voltage <= 3.55):
                 print("Filling up 1st quadrant of battery symbol")
                 self.oled.fill_rect(1, 1, 9, 8, 1)		## Fill the 1st quadrant
-            elif (3.55 <= self.battery_voltage <= 3.70):
+            elif (3.55 <= battery_voltage <= 3.70):
                 print("Filling up 2nd quadrant of battery symbol")
                 self.oled.fill_rect(1, 1, 9, 8, 1)		## Fill the 1st quadrant
                 self.oled.fill_rect(12, 1, 8, 8, 1)		## Fill the 2nd quadrant
-            elif (3.70 <= self.battery_voltage <= 4.0):
+            elif (3.70 <= battery_voltage <= 4.0):
                 print("Filling up 3rd quadrant of battery symbol")
                 self.oled.fill_rect(1, 1, 9, 8, 1)		## Fill the 1st quadrant
                 self.oled.fill_rect(12, 1, 8, 8, 1)		## Fill the 2nd quadrant
                 self.oled.fill_rect(22, 1, 8, 8, 1)		## Fill the 3rd quadrant
-            elif (4.0 <= self.battery_voltage <= 4.2):
+            elif (4.0 <= battery_voltage <= 4.2):
                 print("Filling up 4th quadrant of battery symbol")
                 self.oled.fill_rect(1, 1, 9, 8, 1)		## Fill the 1st quadrant
                 self.oled.fill_rect(12, 1, 8, 8, 1)		## Fill the 2nd quadrant
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C
 import ssd1306
 import time
 from machine import ADC
-from PowerBank import BatteryManager
+from PowerBank import BatteryManager, OledUI
 ## main.py
 ## Author: Sandro Contreras II
 ## License: MIT
@@ -23,37 +23,39 @@ adc = ADC(Pin(26))	## GPIO 26 on Pico is an ADC0 Pin
 percentSymbol = "%"
 battery_percent_str = ""
 
-# BatteryVoltageArr = []
-# movingAvg = []
-#windowSize = 12 
-# i = 0
-# SMA = 0
-# battery_percentage = 0		## Default percentage
-# raw = 0
-# adc_voltage = 0
-# window_average = 0
-# previous_battery_voltage = 0
-# battery_voltage = 0
+i = 0
+SMA = 0
+battery_percentage = 0		## Default percentage
+raw = 0
+adc_voltage = 0
+window_average = 0
+previous_battery_voltage = 0
+battery_voltage = 0
 
         
 BatteryMethods = BatteryManager(raw, adc_voltage, i, SMA, window_average, battery_voltage, battery_percentage)
-OledMethods = OledUI(previous_battery_voltage, battery_percent_str, oled)
+OledMethods = OledUI(previous_battery_voltage, battery_percent_str, oled, battery_voltage, battery_percentage, raw, adc_voltage)
 while True:
     
     previous_battery_voltage = BatteryMethods.PowerCalculator()
     time.sleep(0.5)							# Add a delay between readings for comparison
     battery_voltage = BatteryMethods.PowerCalculator()
     
-    ## Use SMA to smoothen out the battery percentage
-    BatteryMethods.AppendArray(battery_voltage)
-    #BatteryVoltageArr.append(battery_voltage)
-
-    battery_voltage = BatteryMethods.BatteryVoltage_SMA(battery_voltage)
-   
     battery_percentage = BatteryMethods.SOCtable(battery_voltage)
      
     battery_percent_str = str(battery_percentage)
     
     OledMethods.OledSignal(previous_battery_voltage, percentSymbol, battery_voltage, battery_percent_str)
+    
+    ## Use SMA to smoothen out the battery percentage
+    BatteryMethods.AppendArray(battery_voltage)
+
+    battery_voltage = BatteryMethods.BatteryVoltage_SMA(battery_voltage)
+   
+#     battery_percentage = BatteryMethods.SOCtable(battery_voltage)
+#      
+#     battery_percent_str = str(battery_percentage)
+    
+    #OledMethods.OledSignal(previous_battery_voltage, percentSymbol, battery_voltage, battery_percent_str)
     
     OledMethods.variableUpdater(previous_battery_voltage, battery_voltage)	# Update the lower bounds to avoid an always on state
